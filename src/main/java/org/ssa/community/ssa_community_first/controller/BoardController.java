3e304b16c274c0da6c03d3af970d7787a2e85075
seunga <tmddk6522@naver.com>
2021-12-06 16:57:03 +0900

    BoardController, HomeController 및 index.html 생성

---
+++ src/main/java/org/ssa/community/ssa_community_first/controller/BoardController.java
@@ -0,0 +1,4 @@
+package org.ssa.community.ssa_community_first.controller;
+
+public class BoardController {
+}
---
+++ src/main/java/org/ssa/community/ssa_community_first/controller/HomeController.java
@@ -0,0 +1,14 @@
+package org.ssa.community.ssa_community_first.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index/index";
+    }
+
+}
