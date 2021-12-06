65e883560e7fef0599fa66cf3293e434b59112c5
seunga <tmddk6522@naver.com>
2021-12-06 12:03:57 +0900

    ssa_community_first 프로젝트 생성

---
+++ src/main/java/org/ssa/community/ssa_community_first/SsaCommunityFirstApplication.java
@@ -0,0 +1,13 @@
+package org.ssa.community.ssa_community_first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsaCommunityFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsaCommunityFirstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ssa/community/ssa_community_first/SsaCommunityFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ssa.community.ssa_community_first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsaCommunityFirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
