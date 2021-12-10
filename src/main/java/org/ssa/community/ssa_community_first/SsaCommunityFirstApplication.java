b7e12a1e52dd07a73baf2040c298c67667b3aae1
seunga <tmddk6522@naver.com>
2021-12-10 20:07:30 +0900

    썸머노트 관련설정 변경

--- src/main/java/org/ssa/community/ssa_community_first/SsaCommunityFirstApplication.java
+++ src/main/java/org/ssa/community/ssa_community_first/SsaCommunityFirstApplication.java
@@ -2,9 +2,11 @@ package org.ssa.community.ssa_community_first;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.ssa.community.ssa_community_first.entity.TotalBoard;
 
 @SpringBootApplication
-public class SsaCommunityFirstApplication {
+public class SsaCommunityFirstApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SsaCommunityFirstApplication.class, args);
